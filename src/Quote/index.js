d1963df06d346c07500fb5f768f9b1c707434584
Martina Hytychová <martina.hyt@gmail.com>
2020-10-23 18:17:19 +0200

    úprava stylů

--- src/Quote/index.js
+++ src/Quote/index.js
@@ -5,11 +5,11 @@ export const Quote = () => {
   <div class="inner-box">
     <i class="fas fa-quote-left fa2"></i>
   </div>
-  <div class="text">
+  <div class="wrapper">
     <i class="fas fa-quote-right fa1"></i>
-    <div class="wrapper">
-      <h3>Quote the day</h3>
-      <p>
+    <div class="wrapper__text">
+      <h3 class="headline">Quote the day</h3>
+      <p class="text">
         Always code as if the guy who ends up maintaining your code will be
         a violent psychopath who knows where you live
       </p>
